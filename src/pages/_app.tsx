e983fc072d1eab3ee5811714efa842762106e1fa
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-05-26 06:59:17 +0200

    wip

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
-import { useState } from 'react'
 import 'tailwindcss/tailwind.css'
 import client from '@/lib/apollo'
+import * as Fathom from 'fathom-client'
+import { useRouter } from 'next/router'
 import Layout from '@/components/Layout'
 import '@rainbow-me/rainbowkit/styles.css'
+import { useEffect, useState } from 'react'
 import { ApolloProvider } from '@apollo/client'
 import { APP_NAME, IS_MAINNET } from '@/lib/consts'
 import ProfileContext from '@/context/ProfileContext'
@@ -18,7 +20,24 @@ const { connectors } = getDefaultWallets({ appName: APP_NAME, chains })
 const wagmiClient = createClient({ autoConnect: true, connectors, provider })
 
 const App = ({ Component, pageProps }) => {
+	const router = useRouter()
 	const [profile, setProfile] = useState<{ id: string; handle: string; isDefault: boolean }>(null)
+
+	useEffect(() => {
+		Fathom.load('JIZTJTZK', {
+			includedDomains: ['refract.withlens.app'],
+			url: 'https://kangaroo-endorsed.withlens.app/script.js',
+		})
+
+		const onRouteChangeComplete = () => Fathom.trackPageview()
+
+		router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+		return () => {
+			router.events.off('routeChangeComplete', onRouteChangeComplete)
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
 
 	return (
 		<WagmiConfig client={wagmiClient}>
